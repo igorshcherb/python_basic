7f85e7511afdefe4628301cbc73b8c6813d11eaf
igorshcherb <ibshch@yandex.ru>
2024-05-19 20:01:31 +0300

    homework02

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -8,7 +8,7 @@ class Plane(Vehicle):
     cargo: int
     max_cargo: int
 
-    def __init__(self, weight, started, fuel, fuel_consumption, max_cargo):
+    def __init__(self, weight = 100, started = False, fuel = 20, fuel_consumption = 10, max_cargo = 100):
         super().__init__(weight, started, fuel, fuel_consumption)
         self.max_cargo = max_cargo
 
