2fa42c9062ccdf6480bc40254a8f41dd880ad96f
igorshcherb <ibshch@yandex.ru>
2024-05-19 19:34:00 +0300

    homework02

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,27 @@
 from abc import ABC
-
+from exceptions import LowFuelError
 
 class Vehicle(ABC):
-    pass
+    weight: int = 100
+    started: bool = False
+    fuel: int = 20
+    fuel_consumption: int = 10
+def __init__(self, weight, started, fuel, fuel_consumption):
+    self.weight = weight
+    self.started = started
+    self.fuel = fuel
+    self.fuel_consumption = fuel_consumption
+
+def start(self):
+    if not self.started:
+        if self.fuel > 0:
+            self.started = True
+        else:
+            raise LowFuelError('Low fuel')
+def move(self, distance):
+    distance_consumption = (distance / 100) * self.fuel_consumption
+    if self.fuel > distance_consumption:
+        self.fuel -= distance_consumption
+    else:
+        raise NotEnoughFuel('Not enough fuel')
+
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,24 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from base import Vehicle
+from exceptions import CargoOverload
+
+class Plane(Vehicle):
+    cargo: int
+    max_cargo: int
+
+    def __init__(self, weight, started, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, started, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, add_weight):
+        if self.cargo + add_weight <= self.max_cargo:
+            self.cargo -= add_weight
+        else:
+            raise CargoOverload('Cargo overload')
+
+    def remove_all_cargo(self):
+        cargo_before = self.cargo
+        self.cargo = 0
+        return cargo_before
